50a5334c0756dbafc1444e75ee231ee429d599fd
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-03-30 20:10:59 +0200

    add css style to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,15 +5,14 @@ import React from 'react';
 
 export default function Navbar() {
     return (
-        <header>
-            <nav>
+        <header className='border-b border-white/10 font-mono"'>
+            <nav className="max-w-300 h-16 mx-auto px-6 flex items-center justify-between">
                 {/* Logo */}
-                <Link href="/">
+                <Link href="/" className='flex items-center justify-center gap-2'>
                     <Image src="/personal-logo.webp" alt="Logo" width={32} height={32} loading='lazy' />
-                    <span className=''>
-                        Eslam
-                    </span>
+                    <span className="text-white font-semibold text-base">Eslam</span>
                 </Link>
+
                 <ul>
 
                 </ul>
