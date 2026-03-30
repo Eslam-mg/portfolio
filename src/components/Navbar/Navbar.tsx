9ec0225111b70c1d94932b48d4d4236d168bf6a0
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-03-30 18:54:07 +0200

    add logo link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,17 @@
 "use client";
+import Link from 'next/link';
 import React from 'react';
 
 export default function Navbar() {
     return (
         <header>
             <nav>
+                {/* Logo */}
+                <Link href="/">
+                    <span className=''>
+                        Eslam
+                    </span>
+                </Link>
                 <ul>
 
                 </ul>
