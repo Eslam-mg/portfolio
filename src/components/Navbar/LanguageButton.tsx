d2e96cb10c2a848f3d3908e097504021d273cbb9
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-03-31 20:48:10 +0200

    add css style

--- src/components/Navbar/LanguageButton.tsx
+++ src/components/Navbar/LanguageButton.tsx
@@ -11,10 +11,10 @@ export default function LanguageButton() {
                 EN
                 <AngleDownIcon width={16} height={16} className='mt-0.5' />
             </button>
-            <div className={`bg-gray-400 rounded absolute left-0 right-0 top-full translate-y-1 ${isOpen ? "h-14" : "h-0"} overflow-hidden transition-all ease-in-out duration-300`}>
+            <div className={`bg-gray-400 rounded absolute left-0 right-0 top-full translate-y-1 ${isOpen ? "h-14" : "h-0"} overflow-hidden custom-duration`}>
                 <ul className='space-y-1'>
-                    <li className='bg-transparent hover:bg-gray-300 px-1 cursor-pointer transition-colors ease-in-out duration-300 rounded'>EN</li>
-                    <li className='bg-transparent hover:bg-gray-300 px-1 cursor-pointer transition-colors ease-in-out duration-300 rounded'>AR</li>
+                    <li className='bg-transparent hover:bg-gray-300 px-1 cursor-pointer custom-duration rounded'>EN</li>
+                    <li className='bg-transparent hover:bg-gray-300 px-1 cursor-pointer custom-duration rounded'>AR</li>
                 </ul>
             </div>
         </div>
