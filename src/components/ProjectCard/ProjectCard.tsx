ca1457e81bd7801f5adae064e6ad091f12964c09
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-04-04 23:25:17 +0200

    add Image area to projectCard component

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -16,7 +16,20 @@ export default function ProjectCard({
 }: ProjectCardProps) {
     return (
         <div className="border border-borderColor flex flex-col transition-colors hover:border-white/40">
-
+            {/* Image area */}
+            <div className="bg-backgroundColor h-44 relative border-b border-borderColor flex items-center justify-center overflow-hidden">
+                {/* Tech tags */}
+                <div className="absolute top-3 left-3 flex gap-2 flex-wrap">
+                    {technologies.map((tech) => (
+                        <span
+                            key={tech}
+                            className="bg-bg text-gray-text text-[11px] px-2 py-0.5 border border-white/10"
+                        >
+                            {tech}
+                        </span>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 };
