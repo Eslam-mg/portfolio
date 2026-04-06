a334816c129a594833d7c1b1180be823407e138f
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-04-06 22:19:00 +0200

    add props to sectionHeader component

--- src/components/SectionHeader/SectionHeader.tsx
+++ src/components/SectionHeader/SectionHeader.tsx
@@ -3,12 +3,14 @@ interface SectionHeaderProps {
     title: string;
     subtitle?: string;
     className?: string;
+    showLine?: boolean;
 };
 
 export default function SectionHeader({
     title,
     subtitle,
     className = "",
+    showLine = false,
 }: SectionHeaderProps) {
     return (
         <div>SectionHeader</div>
