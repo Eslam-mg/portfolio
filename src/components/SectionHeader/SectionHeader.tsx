dbb41fa11bab4970396f82397dcf8a04ac9820d0
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-04-06 22:25:16 +0200

    add css style

--- src/components/SectionHeader/SectionHeader.tsx
+++ src/components/SectionHeader/SectionHeader.tsx
@@ -15,12 +15,12 @@ export default function SectionHeader({
     return (
         <div className={`mb-10 ${className}`}>
             <h2 className="text-white text-2xl font-semibold m-0 flex items-center font-mono">
-                <span className="text-primary">#</span>
+                <span className="text-primaryColor">#</span>
                 {title}
-                <span className="inline-block ml-3 h-0.5 w-20 bg-primary self-center" />
+                <span className="inline-block ml-3 h-px w-20 bg-primaryColor self-center" />
             </h2>
             {subtitle && (
-                <p className="text-gray-text m-0 mt-2 text-sm">{subtitle}</p>
+                <p className="text-textPrimaryColor m-0 mt-2 text-sm">{subtitle}</p>
             )}
         </div>
     )
