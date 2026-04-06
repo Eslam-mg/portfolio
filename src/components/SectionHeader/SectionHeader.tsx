169231f432826d79dc3255120e94937322247f2f
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-04-06 22:26:41 +0200

    add sectionHeader component

--- src/components/SectionHeader/SectionHeader.tsx
+++ src/components/SectionHeader/SectionHeader.tsx
@@ -17,7 +17,7 @@ export default function SectionHeader({
             <h2 className="text-white text-2xl font-semibold m-0 flex items-center font-mono">
                 <span className="text-primaryColor">#</span>
                 {title}
-                <span className="inline-block ml-3 h-px w-20 bg-primaryColor self-center" />
+                {showLine && (<span className="inline-block ml-3 h-px w-20 bg-primaryColor self-center" />)}
             </h2>
             {subtitle && (
                 <p className="text-textPrimaryColor m-0 mt-2 text-sm">{subtitle}</p>
