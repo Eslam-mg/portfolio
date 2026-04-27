6eb5436ad159b2ad93cba0d141ac836737cc2b35
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-04-27 17:38:41 +0200

    add metadata to about-me apge

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+    title: "About Me | Eslam",
+    description:
+        "Learn more about Eslam — Junior Front-End Developer skilled in React and Next.js, with knowledge of Excel, SQL, and Power BI for data analysis.",
+};
 
 export default function page() {
     return (
