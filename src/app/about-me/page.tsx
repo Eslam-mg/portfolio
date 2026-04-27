05a190393d7432560326ea0217427e76b606ed39
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-04-27 17:34:18 +0200

    add about-me apge

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <div className="font-mono text-gray-text px-6 md:px-12 pt-6 md:pt-12 overflow-hidden space-y-24">page</div>
+    )
+};
