ca9647ee955c623cd20bce81b6a7d2877a3ede34
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-04-29 16:36:04 +0200

    add css style Left — description section to contacts page

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -9,18 +9,18 @@ export const metadata: Metadata = {
 
 export default function page() {
     return (
-        <div className="px-6 md:px-12 pt-6 md:pt-12 overflow-hidden space-y-24 font-mono text-textPrimaryColor">
-            <SectionHeader title="contacts" />
+        <div className="px-6 md:px-12 pt-6 md:pt-12 overflow-hidden font-mono text-textPrimaryColor">
+            <SectionHeader title="contacts" className='mb-8' />
             <div className="grid grid-cols-1 md:grid-cols-2 gap-16 items-start">
                 {/* Left — description */}
-                <div className='space-y-4'>
-                    <p className="text-sm leading-relaxed">
+                <div className='space-y-4 text-base leading-relaxed'>
+                    <p>
                         I'm currently available for freelance projects, full-time roles, part-time opportunities, and remote collaborations in Front-End Development and Data Analysis. 
                     </p>
-                    <p className="text-sm leading-relaxed">
+                    <p>
                         If you have any questions, project ideas, or job opportunities, feel free to get in touch. I’m always open to discussing new challenges and exciting opportunities.
                     </p>
-                    <p className="text-sm leading-relaxed">I’ll do my best to respond to all messages as soon as possible.</p>
+                    <p>I’ll do my best to respond to all messages as soon as possible.</p>
                 </div>
             </div>
         </div>
