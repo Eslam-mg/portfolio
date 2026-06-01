68b878e601db44ff3462a97cf229ff4330a1cfb9
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-06-01 22:05:27 +0200

    add meta data to projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+    title: "projects | Eslam",
+    description:
+        "Explore Eslam's projects in Front-End Development and Data Analysis, featuring React, Next.js, TypeScript, Power BI, SQL, and Excel solutions for building modern applications and extracting valuable insights from data.",
+};
 
 export default function page() {
     return (
