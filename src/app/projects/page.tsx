f83f48ec7e9863e6468302a09c7bcbb5a5caa0cf
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-06-02 23:42:51 +0200

    add ProjecCard commponent to work page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import type { Metadata } from "next";
 import SectionHeader from '@/src/components/SectionHeader/SectionHeader';
+import { myProjects } from './Projects_List';
+import ProjectCard from '@/src/components/ProjectCard/ProjectCard';
 export const metadata: Metadata = {
     title: "projects | Eslam",
     description:
@@ -11,6 +13,19 @@ export default function page() {
     return (
         <div className="font-mono text-gray-text px-6 md:px-12 pt-6 md:pt-12 overflow-hidden space-y-24">
             <SectionHeader title="Projects" subtitle="List of my projects" />
+
+            <div className="">
+                {myProjects.map(project =>
+                    <ProjectCard
+                        key={project.id}
+                        title={project.title}
+                        description={project.description}
+                        technologies={project.technologies}
+                        githubUrl={project.githubUrl}
+                        liveUrl={project.liveUrl}
+                    />
+                )}
+            </div>
         </div>
     )
 };
