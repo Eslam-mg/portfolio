b8c5e2b42137ea540a1571d1c4b390e901d6d148
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-06-04 14:59:28 +0200

    split description into short and full

--- src/app/projects/Projects_List.tsx
+++ src/app/projects/Projects_List.tsx
@@ -2,7 +2,7 @@ export const myProjects = [
     {
         id: 1,
         title: "Ecommerce Next.js",
-        shortDescription: "A modern e-commerce platform with product browsing, shopping cart, wishlist, authentication, and a fully responsive user experience.",
+        shortDescription: "E-commerce platform with product browsing, shopping cart, wishlist, authentication, and a fully responsive user experience.",
         fullDescription: "Developed a full-stack e-commerce application using Next.js and TypeScript, delivering a fast and scalable shopping experience. The project includes product listing and filtering, detailed product pages, shopping cart management, wishlist functionality, user authentication, and responsive layouts for all devices. Built with React and Tailwind CSS to ensure maintainable code, reusable components, and a modern user interface while leveraging Next.js features for performance optimization and routing.",
         technologies: ["Next.js", "TypeScript", "React", "Tailwind CSS"],
         githubUrl: "https://github.com/Eslam-mg/Ecommerce-Next.tsx",
@@ -22,7 +22,7 @@ export const myProjects = [
     {
         id: 2,
         title: "Supply Chain Dashboard",
-        shortDescription: "An interactive dashboard providing insights into sales performance, inventory management, supplier efficiency, and key supply chain metrics.",
+        shortDescription: "Interactive dashboard providing insights into sales performance, inventory management, supplier efficiency, and key supply chain metrics.",
         fullDescription: "Designed and developed a Supply Chain Analytics Dashboard to transform raw business data into actionable insights. The dashboard analyzes sales trends, inventory levels, supplier performance, shipping operations, and key operational KPIs. Data was cleaned, transformed, and modeled using SQL, Python, and Power Query before being visualized in Power BI. Interactive reports and drill-down features enable stakeholders to monitor performance, identify bottlenecks, and support data-driven decision making.",
         technologies: ["Python", "SQL", "Power Query", "Power BI"],
         githubUrl: "https://github.com/Eslam-mg/Data-Analysis-Learning/tree/main/supply%20chain",
