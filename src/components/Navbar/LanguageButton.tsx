5927587a7ea14e9c138fa79efe7564b0b620ea4a
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-06-18 16:29:18 +0200

    add css style

--- src/components/Navbar/LanguageButton.tsx
+++ src/components/Navbar/LanguageButton.tsx
@@ -20,11 +20,11 @@ export default function LanguageButton() {
 
     return (
         <div className="relative" ref={toggleRef}>
-            <button onClick={() => setIsOpen(!isOpen)} type='button' aria-label='Button to change language' className='flex items-center justify-around border border-borderColor rounded w-14 h-8 cursor-pointer'>
+            <button onClick={() => setIsOpen(!isOpen)} type='button' aria-label='Button to change language' className='flex items-center justify-around border border-borderColor w-14 h-8 cursor-pointer'>
                 {language}
                 <AngleDownIcon width={16} height={16} className='mt-0.5' />
             </button>
-            <div className={`bg-gray-400 rounded absolute left-0 right-0 top-full translate-y-1 ${isOpen ? "h-14" : "h-0"} overflow-hidden custom-duration`}>
+            <div className={`bg-backgroundColor absolute left-0 right-0 top-full translate-y-1 ${isOpen ? "h-14 border border-borderColor" : "h-0"} overflow-hidden custom-duration`}>
                 <ul className='space-y-1'>
                     <li onClick={() => setLanguage("EN")} className='bg-transparent hover:bg-gray-300 px-1 cursor-pointer custom-duration rounded'>EN</li>
                     <li onClick={() => setLanguage("AR")} className='bg-transparent hover:bg-gray-300 px-1 cursor-pointer custom-duration rounded'>AR</li>
