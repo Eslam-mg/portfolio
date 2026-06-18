9a5905c8261d2a91bf59ed1e0e67216338210c5e
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-06-18 18:12:17 +0200

    handel language button

--- src/components/Navbar/LanguageButton.tsx
+++ src/components/Navbar/LanguageButton.tsx
@@ -20,14 +20,14 @@ export default function LanguageButton() {
 
     return (
         <div className="relative" ref={toggleRef}>
-            <button onClick={() => setIsOpen(!isOpen)} type='button' aria-label='Button to change language' className='flex items-center justify-around border border-borderColor w-14 h-8 cursor-pointer'>
+            <button onClick={() => setIsOpen(!isOpen)} type='button' aria-label='Button to change language' className='flex items-center justify-around text-textPrimaryColor border border-borderColor w-14 h-8 cursor-pointer'>
                 {language}
                 <AngleDownIcon width={16} height={16} className='mt-0.5' />
             </button>
             <div className={`bg-backgroundColor absolute left-0 right-0 top-full translate-y-1 ${isOpen ? "h-14 border border-borderColor" : "h-0"} overflow-hidden custom-duration`}>
                 <ul className='space-y-1'>
-                    <li onClick={() => setLanguage("EN")} className='bg-transparent hover:bg-gray-300 px-1 cursor-pointer custom-duration rounded'>EN</li>
-                    <li onClick={() => setLanguage("AR")} className='bg-transparent hover:bg-gray-300 px-1 cursor-pointer custom-duration rounded'>AR</li>
+                    <li onClick={() => {setLanguage("EN"); setIsOpen(false)}} className={`${language === "EN" ? "text-white bg-primaryColor/10 border-l-2 border-primaryColor": "text-textPrimaryColor hover:bg-white/5 hover:text-white border-l-2 border-transparent" } px-1 cursor-pointer custom-duration`}>EN</li>
+                    <li onClick={() => {setLanguage("AR"); setIsOpen(false)}} className={`${language === "AR" ? "text-white bg-primaryColor/10 border-l-2 border-primaryColor": "text-textPrimaryColor hover:bg-white/5 hover:text-white border-l-2 border-transparent" } px-1 cursor-pointer custom-duration`}>AR</li>
                 </ul>
             </div>
         </div>
