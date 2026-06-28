1e0cf2e7b5062b4aeaf7bfcf5e250c984d2933ae
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-06-28 19:15:17 +0200

    add props type

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { myProjects } from '../Projects_List';
 
+type PageProps = {
+    params: Promise<{ id: string }>;
+};
+
 export async function generateStaticParams() {
     return myProjects.map((project) => ({
         id: String(project.id),
     }));
 }
 
-export default async function page() {
+
+export default async function page({ params }: PageProps) {
     return (
         <div>page</div>
     )
